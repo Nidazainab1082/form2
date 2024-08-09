1a7d0f885e84954486b8abf557158a31251d190a
Nidazainab1082 <nidazainab1082@gmail.com>
2024-08-09 00:33:22 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+const button = document.getElementById('myButton');
+
+// Define the function to be called when the button is clicked
+function handleClick() {
+    alert('Button clicked!');
+}
+
+// Attach the function to the button's click event
+button.addEventListener('click', handleClick);
+// JavaScript to handle button click event
+document.getElementById('welcomeButton').addEventListener('click', function() {
+    alert('Hello and welcome to my website!');
+});
+
+
+
+
